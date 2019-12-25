6ff4336bc50353690aba634d336e76ef0d375424
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2019-12-25 09:01:44 -0400

    Diseño Scroll - parte 2 #5
    Detalle de los widgets usados:
    
    + Stack: permite poner un widget sobre otro

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -4,25 +4,56 @@ class ScrollPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          _pagina1(),
-          _pagina2()
-        ],
-      )
-    );
+        body: PageView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[_pagina1(), _pagina2()],
+    ));
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
 
   Widget _pagina2() {
     return Center(
       child: Text('Pagina 2'),
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(image: AssetImage('assets/scroll-1.png'), fit: BoxFit.cover,),
+    );
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0,),
+          Text('11 º', style: estiloTexto,),
+          Text('Miércoles ', style: estiloTexto,),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
     );
   }
 }
