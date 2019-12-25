384a4bdcd7bf472e75e0174f6aac7829126d069a
DentVega <dentvega6@gmail.com>
2019-12-25 02:23:33 -0400

    - Diseño basico - parte 3
    Detalle de los widgets usados:
    
    - Row: Sirve para poner una lista de widget en filas
    - MainAxisAlignment: afecta la alineacion Primaria de una columna o Fila que en este caso seria lo horizontal, con el crossAxis puedes definir donde o como quieres que se aline  los componentes de la columna o la fila, en este caso usamo el spaceEvenly que distribuye el espacio entre los elementos igualitariamente
    
    - EdgeInsets: nos ayuda a generar spacio como el padding de un component

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -9,42 +9,96 @@ class BasicoPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image(
-              image: NetworkImage(
-                  'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Row(
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearText(),
+
+
+        ],
+      ),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'));
+  }
+
+  Widget _crearTitulo() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Lago con un puente',
-                        style: estiloTitulo,
-                      ),
-                      SizedBox(height: 7.0,),
-                      Text(
-                        'Un lago en Alemania',
-                        style: estiloSubTitulo,
-                      ),
-                    ],
-                  ),
+                Text(
+                  'Lago con un puente',
+                  style: estiloTitulo,
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.red,
-                  size: 30.0,
+                SizedBox(
+                  height: 7.0,
                 ),
                 Text(
-                  '41',
-                  style: TextStyle(fontSize: 20.0),
-                )
+                  'Un lago en Alemania',
+                  style: estiloSubTitulo,
+                ),
               ],
             ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+            size: 30.0,
+          ),
+          Text(
+            '41',
+            style: TextStyle(fontSize: 20.0),
           )
         ],
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _crearAccion(Icons.call, 'CALL'),
+        _crearAccion(Icons.near_me, 'ROUTE'),
+        _crearAccion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _crearAccion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          texto,
+          style: TextStyle(fontSize: 15.0, color: Colors.blue),
+        )
+      ],
+    );
+  }
+
+  Widget _crearText() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Marshmellow platter has to have a crushed, grey caviar component. Marshmellow platter has to have a crushed, grey caviar component. Marshmellow platter has to have a crushed, grey caviar component. Marshmellow platter has to have a crushed, grey caviar component. Marshmellow platter has to have a crushed, grey caviar component. Marshmellow platter has to have a crushed, grey caviar component. Marshmellow platter has to have a crushed, grey caviar component.',
+        textAlign: TextAlign.justify,
       ),
     );
   }
