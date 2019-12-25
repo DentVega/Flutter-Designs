0f2f906b5026623e3f89f67233679f732910d234
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2019-12-25 11:10:11 -0400

    Diseño Compuesto - BottomNavigationBar
    Widgets Utilizados
    
    - en el Scaffold activamos el BottomNavigationBar que recibe una lista de BottomNavigationBarItem
    
    - Para modificar solo el color del BottomNavigationBar sin afectar los colores de toda la app, encerramos el BottomNavigationBar dentro de un Theme que copia el theme principal de la app pero a la vez te permite modificarlo para que su hijo tengo propiedades personalizadas

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -20,6 +20,7 @@ class BotonesPage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: _bottomNavigationBar(context),
     );
   }
 
@@ -70,12 +71,36 @@ class BotonesPage extends StatelessWidget {
                   fontSize: 30.0,
                   fontWeight: FontWeight.bold),
             ),
-            SizedBox(height:  10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             Text('Classify this transaction into a particular category',
                 style: TextStyle(color: Colors.white, fontSize: 18.0))
           ],
         ),
       ),
     );
+  }
+
+  Widget _bottomNavigationBar(BuildContext context) {
+    return new Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+            primaryColor: Colors.pinkAccent,
+            textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))),
+        child: BottomNavigationBar(items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.calendar_today,
+                size: 30.0,
+              ),
+              title: Container()),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bubble_chart, size: 30.0), title: Container()),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle, size: 30.0),
+              title: Container())
+        ]));
   }
 }
